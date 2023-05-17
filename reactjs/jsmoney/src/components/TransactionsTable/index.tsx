7b54719f58192ab54650471112749794e10fa1f9
jonatasNs1 <jonatassantos152409@gmail.com>
2023-05-17 15:55:48 -0300

    formando falores

--- reactjs/jsmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/jsmoney/src/components/TransactionsTable/index.tsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction{
+    id: number;
+    title: string;
+    description: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable(): JSX.Element{
    
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return(
@@ -22,18 +34,26 @@ export function TransactionsTable(): JSX.Element{
                 </thead>
                 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento</td>
-                        <td className="deposit">R$ 100,00</td>
-                        <td>Alimentação</td>
-                        <td>20/03/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$ 200,00</td>
-                        <td>Alimentação</td>
-                        <td>20/03/2021</td>
-                    </tr>
+                   {
+                    transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                                </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                            {new Intl.DateTimeFormat('pt-BR', {}).format(
+                            new Date(transaction.createdAt)
+                                )}
+                            
+                            </td>
+                        </tr>
+                    ))
+                   }
                 </tbody>
             </table>
        </Container>
--- reactjs/jsmoney/src/index.tsx
+++ reactjs/jsmoney/src/index.tsx
@@ -1,48 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {createServer, Model} from 'miragejs';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs'
+import App  from './App';
 
 createServer({
 
-  models:{
+  models: {
     transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        },
+      ],
+    })
   },
 
   routes() {
     this.namespace = 'api';
 
-    this.get('/transactions', () =>{
-      return[
-        {
-          id: 1,
-          description: 'Transaction 1'
-        },
-        {
-          id: 2,
-          description: 'Transaction 2'
-        },
-        {
-          id: 3,
-          description: 'Transaction 3'
-        }
-      ]
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
     })
-   this.post('/transactions', (schema, request) =>{
-    const data = JSON.parse(request.requestBody)
-    
-    return data;
-   })
-  },
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    })
+  }
 })
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-
